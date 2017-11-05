8510e8a0253af54b22cf92d88e778a7ee5bbec3d
Felipe Marques <lipemarques8@gmail.com>
2017-11-05 09:39:51 -0300

    Loading the test data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
+import data from './testData'
+console.log('====================================')
+console.log(data)
+console.log('====================================')
 
 ReactDOM.render(
-  <App />,
+  <App contests={data.contests}/>,
   document.getElementById('root')
 )
